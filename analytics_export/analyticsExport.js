613ae2c64b9d0c066fe73b55ce2c86d40e257a74
Benjamin Wu <benjaminwu124@gmail.com>
2020-06-27 00:08:19 -0400

    Create export script

--- analytics_export/analyticsExport.js
+++ analytics_export/analyticsExport.js
@@ -1,8 +1,43 @@
-"use strict";
+'use strict';
 
-function startExport() {
-    console.log("test");
-    return "test";
+const util = require('util');
+
+// Retrieve list of measures and dimensions
+async function getAnalyticsMetadata(client, date) {
+  const getSettings = util.promisify(client.getSettings).bind(client);
+  const settings = await getSettings();
+
+  const executionDate = Date.parse(date);
+  const dataEndDate = settings.configuration.dataEndDate.slice(0, 10);
+  const dataStartDate = settings.configuration.dataStartDate.slice(0, 10);
+
+  if (executionDate > Date.parse(dataEndDate) || executionDate < Date.parse(dataStartDate)) {
+    throw Error(`No data found for ${date}; data exists for ${dataStartDate} to ${dataEndDate}`);
+  }
+
+  const dimensions = {};
+  settings.dimensions.forEach((dimension) => {
+    dimensions[dimension.id] = dimension;
+  });
+
+  const measures = {};
+  settings.measures.forEach((measure) => {
+    measures[measure.id] = measure;
+  });
+
+  return {
+    dimensions,
+    measures,
+  };
+}
+
+async function startExport(client, appid, date) {
+  console.log('Starting export');
+
+  const metadata = await getAnalyticsMetadata(client, date)
+    .catch((err) => { throw Error(`Failed to get analytics metadata: ${err}`); });
+
+  util.debug(metadata);
 }
 
 exports.startExport = startExport;
