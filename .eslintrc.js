28f9dcdea6891b81ceeea4f0ac0cda6b4eea39c2
Benjamin Wu <benjaminwu124@gmail.com>
2020-07-09 16:43:42 -0400

    Add prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,20 +6,13 @@ module.exports = {
   },
   extends: [
     'airbnb-base',
+    'prettier',
   ],
   parserOptions: {
     ecmaVersion: 11,
     sourceType: 'script',
   },
   rules: {
-    'max-len': [
-      'warn',
-      {
-        code: 100,
-        ignoreStrings: true,
-        ignoreTemplateLiterals: true,
-      },
-    ],
     'no-await-in-loop': 0,
     'no-console': 0,
     'no-continue': 0,
